1d9938bea945759ecc591917ff327cc7a38de462
ThatSlyGuy <matthew121250@hotmail.com>
2018-08-31 12:19:58 -0400

    First commit
    - Added projects, references, and initial classes

---
+++ Hope.Ethereum.PRPS/Hope.Ethereum.PRPS/src/Token Contracts/DUBIToken.cs
@@ -0,0 +1,23 @@
+﻿using Hope.Ethereum.Tokens;
+
+namespace Hope.Ethereum.PRPS
+{
+    public sealed class DUBIToken : ERC20
+    {
+        public DUBIToken(string mainnetAddress, string rinkebyAddress) : base(mainnetAddress, rinkebyAddress)
+        {
+        }
+
+        public DUBIToken(string mainnetAddress) : base(mainnetAddress)
+        {
+        }
+
+        public DUBIToken(string mainnetAddress, string name, string symbol, int decimals) : base(mainnetAddress, name, symbol, decimals)
+        {
+        }
+
+        public DUBIToken(string mainnetAddress, string rinkebyAddress, string name, string symbol, int decimals) : base(mainnetAddress, rinkebyAddress, name, symbol, decimals)
+        {
+        }
+    }
+}
---
+++ Hope.Ethereum.PRPS/Hope.Ethereum.Unity.PRPS/src/Token Contracts/PRPSToken.cs
@@ -0,0 +1,23 @@
+﻿using Hope.Ethereum.Unity.Tokens;
+
+namespace Hope.Ethereum.Unity.PRPS
+{
+    public sealed class PRPSToken : ERC20
+    {
+        public PRPSToken(string mainnetAddress, string rinkebyAddress) : base(mainnetAddress, rinkebyAddress)
+        {
+        }
+
+        public PRPSToken(string mainnetAddress) : base(mainnetAddress)
+        {
+        }
+
+        public PRPSToken(string mainnetAddress, string name, string symbol, int decimals) : base(mainnetAddress, name, symbol, decimals)
+        {
+        }
+
+        public PRPSToken(string mainnetAddress, string rinkebyAddress, string name, string symbol, int decimals) : base(mainnetAddress, rinkebyAddress, name, symbol, decimals)
+        {
+        }
+    }
+}
