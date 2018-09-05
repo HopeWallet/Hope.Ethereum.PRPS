eaeb6b2e4cd1f32777e059da04e3dae4188e2b21
ThatSlyGuy <matthew121250@hotmail.com>
2018-09-05 12:14:09 -0400

    Added Hodler and mainnet/rinkeby addresses for all contracts

---
+++ Hope.Ethereum.PRPS/Hope.Ethereum.PRPS/src/Token Contracts/DUBI.cs
@@ -0,0 +1,14 @@
+﻿using Hope.Ethereum.Tokens;
+
+namespace Hope.Ethereum.PRPS
+{
+    public sealed class DUBI : ERC20
+    {
+        private const string DUBI_MAINNET = "0x9c6Fa42209169bCeA032e401188a6fc3e9C9f59c";
+        private const string DUBI_RINKEBY = "0x8b069Ecf7BF230E153b8Ed903bAbf24403ccA203";
+
+        public DUBI() : base(DUBI_MAINNET, DUBI_RINKEBY, "Decentralized Universal Basic Income", "DUBI", 18)
+        {
+        }
+    }
+}
---
+++ Hope.Ethereum.PRPS/Hope.Ethereum.PRPS/src/Token Contracts/PRPS.cs
@@ -0,0 +1,14 @@
+﻿using Hope.Ethereum.Tokens;
+
+namespace Hope.Ethereum.PRPS
+{
+    public sealed class PRPS : ERC20
+    {
+        private const string PRPS_MAINNET = "0xd94F2778e2B3913C53637Ae60647598bE588c570";
+        private const string PRPS_RINKEBY = "0x5831819C84C05DdcD2568dE72963AC9f1e6831b6";
+
+        public PRPS() : base(PRPS_MAINNET, PRPS_RINKEBY, "Purpose", "PRPS", 18)
+        {
+        }
+    }
+}
